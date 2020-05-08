4c61a6a0c39bae5f67f2303e861a5f2b7c6c464e
Firstname Lastname <chou@simmons.edu>
2020-05-07 21:31:50 -0400

    Toggles CSS class if user selects new sortByOption

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import './SearchBar.css';
 
-const sortByOptions = {
-    'Best Match': 'best_match',
-    'Highest Rated': 'rating',
-    'Most Reviewed': 'review_count'
-};
-
 class SearchBar extends React.Component{
     constructor(props){
         super(props);
         this.state = { term: '', location: '', sortBy: 'best_match' };
+        this.sortByOptions = {
+            'Best Match': 'best_match',
+            'Highest Rated': 'rating',
+            'Most Reviewed': 'review_count'
+        };
+    }
+
+    getSortByClass(sortByOption){
+        return this.state.sortBy == sortByOption ? 'active' : '';
+    }
+
+    handleSortByChange(sortByOption){
+        this.setState( {sortBy: sortByOption} );
     }
 
     renderSortByOptions(){ //dynamically creates list items needed to display sort options
-        return Object.keys(sortByOptions).map(sortByOption => {
-            let sortByOptionValue = sortByOptions[sortByOption];
-            return <li key="{sortByOptionValue}">{sortByOption}</li>;
+        return Object.keys(this.sortByOptions).map(sortByOption => {
+            let sortByOptionValue = this.sortByOptions[sortByOption];
+            return (<li 
+                onClick={this.handleSortByChange.bind(this,sortByOptionValue)} //binds the current value of this and binds the current sortByOptionValue
+                key="{sortByOptionValue}" 
+                className={this.getSortByClass(sortByOptionValue)}>
+                {sortByOption}</li> //styles sort by option, displays to user current selected sort
+            );
         });
     }
 
