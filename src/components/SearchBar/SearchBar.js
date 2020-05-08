40924e6d27c4f958920f602ea9c5a2d22101607e
Firstname Lastname <chou@simmons.edu>
2020-05-07 22:04:27 -0400

    Allow input for location and business in SearchBar

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -5,6 +5,8 @@ class SearchBar extends React.Component{
     constructor(props){
         super(props);
         this.state = { term: '', location: '', sortBy: 'best_match' };
+        this.handleTermChange = this.handleTermChange.bind(this);
+        this.handleLocationChange = this.handleLocationChange.bind(this);
         this.sortByOptions = {
             'Best Match': 'best_match',
             'Highest Rated': 'rating',
@@ -18,6 +20,14 @@ class SearchBar extends React.Component{
 
     handleSortByChange(sortByOption){
         this.setState( {sortBy: sortByOption} );
+    }
+
+    handleTermChange(event){
+        this.setState({term: event.target.value});
+    }
+
+    handleLocationChange(event){
+        this.setState({location: event.target.value});
     }
 
     renderSortByOptions(){ //dynamically creates list items needed to display sort options
@@ -41,8 +51,8 @@ class SearchBar extends React.Component{
                     </ul>
                 </div>
                 <div className="SearchBar-fields">
-                    <input placeholder="Search Businesses" />
-                    <input placeholder="Where?" />
+                    <input onChange={this.handleTermChange} placeholder="Search Businesses" />
+                    <input onChange={this.handleLocationChange} placeholder="Where?" />
                 </div>
                 <div className="SearchBar-submit">
                     <a>Let's Go</a>
