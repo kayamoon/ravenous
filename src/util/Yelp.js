c5b6aa0e3edb299e96647c5853955d1c21db47de
Firstname Lastname <chou@simmons.edu>
2020-05-11 18:59:06 -0400

    Fetch business data from Yelp API

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,2 +1,32 @@
 import { config } from './config';
 
+export const Yelp = {
+    search(term, location, sortBy) {
+        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
+        {
+            headers: {
+                Authorization: `Bearer ${config.YELP_API_KEY}`
+            }
+        }).then(response => {
+            return response.json();
+        },jsonResponse => {
+            if(jsonResponse.businesses){
+                return jsonResponse.businesses.map(business => {
+                    business = {
+                        id: business.id,
+                        imageSrc: business.image_url,
+                        name: business.name,
+                        address: business.location.display_address,
+                        city: business.location.city,
+                        state: business.location.state,
+                        zipCode: business.location.zip_code,
+                        category: business.categories,
+                        rating: business.rating,
+                        reviewCount: business.review_count
+                    }
+                })
+            }
+        })
+    }
+}
+
