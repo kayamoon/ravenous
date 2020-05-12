82e1bb0c1dc762eb725ba8c691b07609547cd37a
Firstname Lastname <chou@simmons.edu>
2020-05-11 21:46:14 -0400

    Fetched business data from Yelp API

--- src/util/Yelp.js
+++ src/util/Yelp.js
@@ -1,32 +1,30 @@
-import { config } from './config';
+import config from './config';
 
-export const Yelp = {
-    search(term, location, sortBy) {
-        return fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`,
-        {
+const Yelp = {
+    async search(term, location, sortBy) {
+        const response = await fetch(`https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${term}&location=${location}&sort_by=${sortBy}`, {
             headers: {
                 Authorization: `Bearer ${config.YELP_API_KEY}`
             }
-        }).then(response => {
-            return response.json();
-        },jsonResponse => {
-            if(jsonResponse.businesses){
-                return jsonResponse.businesses.map(business => {
-                    business = {
-                        id: business.id,
-                        imageSrc: business.image_url,
-                        name: business.name,
-                        address: business.location.display_address,
-                        city: business.location.city,
-                        state: business.location.state,
-                        zipCode: business.location.zip_code,
-                        category: business.categories,
-                        rating: business.rating,
-                        reviewCount: business.review_count
-                    }
-                })
-            }
-        })
+        });
+        const jsonResponse = await response.json();
+        if (jsonResponse.businesses) {
+            return jsonResponse.businesses.map(business => {
+                return {
+                    id: business.id,
+                    imageSrc: business.image_url,
+                    name: business.name,
+                    address: business.location.address1,
+                    city: business.location.city,
+                    state: business.location.state,
+                    zipCode: business.location.zip_code,
+                    category: business.categories[0].title,
+                    rating: business.rating,
+                    reviewCount: business.review_count
+                };
+            });
+        }
     }
 }
 
+export default Yelp;
