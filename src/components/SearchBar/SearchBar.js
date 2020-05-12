bbffad664cf98b824818abb2cf06ad450af14242
Firstname Lastname <chou@simmons.edu>
2020-05-11 22:38:20 -0400

    Render new search upon changing sort by option

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -21,7 +21,10 @@ class SearchBar extends React.Component{
     }
 
     handleSortByChange(sortByOption){
-        this.setState( {sortBy: sortByOption} );
+        this.setState({sortBy: sortByOption
+        },() => {
+            this.props.searchYelp(this.state.term, this.state.location, this.state.sortBy); //handles search upon changing sort by option
+        });
     }
 
     handleTermChange(event){
